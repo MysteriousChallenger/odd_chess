b098554a6f475973b0662ce63d25c1abd17e6299
asdf <asdf>
2019-09-27 12:07:32 -0700

    readme edits

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-odd_chess
+#  **Odd Chess is a Chess Server built for the F2019 ICS-60 course @ UCI**  
+  
+###Quick Summary:
+- Written in Python using the Django library
+- Built for hosting on Heroku (in fact heavily reuses their Django/Python [getting started](https://github.com/heroku/python-getting-started) repo)
--- hello/views.py
+++ hello/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import Greeting
+import requests
 
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
     return render(request, "index.html")
+
+def test(request):
+    r = requests.get('http://httpbin.org/status/418')
+    return HttpResponse('<pre>' + r.text + '</pre>')
 
 
 def db(request):
